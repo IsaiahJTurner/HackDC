51e1d5db88730fce34a830002edeaf3b41058b45
Isaiah J Turner <github.com@isaiahjturner.com>
2015-09-27 08:48:34 -0400

    Last Minute Fixes

---
+++ iMonitor/PatientInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PatientInfoViewController.h
+//  iMonitor
+//
+//  Created by Isaiah Turner on 9/27/15.
+//  Copyright © 2015 Firebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PatientInfoViewController : UIViewController
+
+@end
---
+++ iMonitor/PatientInfoViewController.m
@@ -0,0 +1,13 @@
+//
+//  PatientInfoViewController.m
+//  iMonitor
+//
+//  Created by Isaiah Turner on 9/27/15.
+//  Copyright © 2015 Firebase. All rights reserved.
+//
+
+#import "PatientInfoViewController.h"
+
+@implementation PatientInfoViewController
+    
+@end
